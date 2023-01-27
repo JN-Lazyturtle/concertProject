630394a89c103e8137f2cfc61d5f0362df987104
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-01-27 15:05:05 +0100

    nettoyage fixtures

--- skeleton/src/DataFixtures/ConcertFixture.php
+++ skeleton/src/DataFixtures/ConcertFixture.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Concert;
-use App\Entity\ConcertHall;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
